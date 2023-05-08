ed1190ab291e794db9b69869366e7449489f6eae
Klevert Opee <klevertope@gmail.com>
2023-05-08 21:55:07 +0300

    commit

---
+++ src/js/motion.js
@@ -0,0 +1,44 @@
+import { gsap } from "gsap";
+import { ExpoScaleEase, RoughEase, SlowMo } from "gsap/EasePack";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, ExpoScaleEase, RoughEase, SlowMo);
+
+//header motion
+gsap.from(".name", {opacity: 0, y: -10, duration: 4, ease: 'power2'});
+gsap.from(".tag", {opacity: 0, x: -10, duration: 4.5, ease: 'power2'});
+gsap.from(".nav", {opacity: 0, x: 10, duration: 4.5, ease: 'power2'});
+
+//footer motion
+gsap.from(".symbol", {opacity: 0, y: 10, duration: 3, ease: 'power2'});
+gsap.from(".brandmotto", {opacity: 0, x: -10, duration: 3.5, ease: 'power2'});
+gsap.from(".social", {opacity: 0, x: -10, duration: 4.8, ease: 'power2'});
+gsap.from(".contact", {opacity: 0, x: -10, duration: 4.8, ease: 'power2'});
+
+//showcase motion
+gsap.from(".reveal", {opacity: 0, x: -80, duration: 5.8, ease: 'power2'});
+
+// select elements to reveal
+const elements = document.querySelectorAll('.revealscroll');
+
+// set initial y position below viewport
+gsap.set(elements, { y: 10});
+
+// reveal elements when they come into view
+elements.forEach(el => {
+  gsap.to(el, {
+    y: -10,
+    duration: 2.5,
+    ease: 'power2',
+    scrollTrigger: {
+      trigger: el,
+      start: 'top 90%',
+    },
+  });
+});
+
+
+
+
