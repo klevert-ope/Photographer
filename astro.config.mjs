49c4019f5344605b30a93b7370883fe415e60a36
Klevert Opee <klevertope@gmail.com>
2023-08-31 15:38:39 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({  
-  integrations: [tailwind(), compress()]
+export default defineConfig({
+  integrations: [tailwind()],
 });
